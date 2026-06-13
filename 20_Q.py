c1a6d5bf18b65976604a1c01032055cb078e63b1
Jake Rothacker <jakerothacker@gmail.com>
2026-06-13 15:13:22 -0400

    experimental 20_Q.py with deduction ablility

--- 20_Q.py
+++ 20_Q.py
@@ -1,4 +1,5 @@
-cat: dict[str,bool] = {
+cat: dict[str,any] = {
+    "name": "cat",
     "Is it physical?": True,
     "Is it an animal?": True,
     "Does it have four legs?": True,
@@ -7,7 +8,8 @@ cat: dict[str,bool] = {
     "Is it a feline?": True,
     "Can it be brown?": True
 }
-dog: dict[str,bool] = {
+dog: dict[str,any] = {
+    "name": "dog",
     "Is it physical?": True,
     "Is it an animal?": True,
     "Does it have four legs?": True,
@@ -18,7 +20,8 @@ dog: dict[str,bool] = {
 }
 
 possible_answers = [cat, dog]
-possible_questions = list (cat.keys())
+possible_questions = list(cat.keys())
+possible_questions.pop(0)
 game_is_going = True
 count = 0
 response = ""
@@ -26,17 +29,22 @@ answer: bool = True
 
 
 while game_is_going:
-  
-    while response != "T" or "F":
-        response = input("T or F: " + possible_questions[count])
-        if response == "T":
-            answer = True
-        elif response == "F":
-            answer = False
-        elif response == "Quit" or response == "quit":
-            game_is_going = False
-            break
-        else:
-            print("Invalid input: Please enter T or F")
+
+    response = input("T or F: " + possible_questions[count])
+    if response == "T":
+        answer = True
+    elif response == "F":
+        answer = False
+    elif response == "Quit" or response == "quit":
+        game_is_going = False
+        break
+    else:
+        print("Invalid input: Please enter T or F")
+
+    for i in range(len(possible_answers)):
+        if possible_answers[i][possible_questions[count]] != answer:
+            possible_answers.pop(i)
         
-    
+    count += 1
+    for i in range(len(possible_answers)):
+        print(possible_answers[i]["name"] + " is still possible")
